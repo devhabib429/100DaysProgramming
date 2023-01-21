67edb65f3de6d52b7093a7820ade3f18d9a06e01
Habibur <habibur@nestorbird.com>
2023-01-21 12:22:30 +0530

    DAY-9: Convert Lower to Upper Case vice-versa

---
+++ uppertolower_case.c
@@ -0,0 +1,20 @@
+// Write a Program to convert from UpperCase to LowerCase for a String ?
+#include <stdio.h>
+int main()
+{
+    char s[20];
+    int l=0;
+    char c;
+    int x;
+    int i,j;
+    printf("Enter a string : ");
+    scanf("%[^\n]s",s);
+    printf("\nEntered string is %s",s);
+    for(i=0;s[i]!='\0';i++){
+       s[i] = s[i] + 32; 
+    }
+    printf("\nString Capitilize is %s",s);
+    
+    return 0;
+}
+

d44c6512952b8808fd57f7686d5db2ad0f4bd2c0
Habibur <habibur@nestorbird.com>
2023-01-29 21:27:18 +0530

    DAY-11: String in Desc

---
+++ str_in_desc.c
@@ -0,0 +1,24 @@
+//C Program to sort in desc character in string
+#include <stdio.h>
+#include <string.h>
+int main ()
+{
+  char str[100];
+  char temp;
+	int i, j,len;
+  printf("C Program to sort character in string\n");
+  printf("Please enter the string : ");
+  scanf("%[^\n]",str);
+ len = strlen(str);
+	for (i = 0; i < len-1; i++) {
+		for (j = i+1; j < len; j++) {
+			if (str[i] < str[j]) {
+					temp = str[i];
+					str[i] = str[j];
+					str[j] = temp;
+			}
+		}
+	}
+	printf("After sorting the character in ascending : %s", str);
+	return 0;
+}
